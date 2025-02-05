fe892334e18ff8214da994d8d6523cccd2df3ca7
aj1441 <167340937+aj1441@users.noreply.github.com>
2025-02-05 13:42:31 -0700

    hot fix for production

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,15 @@ export default function Main(props) {
     const { data } = props
     return (
         <div className="imgContainer">
-            <img src={data.hdurl} alt={data.title || 'bg-img'} className="bgImage" />
-        </div>
+            {data.media_type === 'video' ? (
+                <iframe
+                    id="inlineFrameExample"
+                    title={data.title}
+                    className="bgImage"
+                    src={data.url}>
+                </iframe>) : (
+                    <img src={data.hdurl} alt={data.title || 'bg-img'} className="bgImage" />
+        )}
+ </div >
     )
 }
