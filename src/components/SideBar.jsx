d51b7e05d3903c843f161cad97c5ceb744a34c5a
PhillipDaum <philgdaum@gmail.com>
2025-02-05 15:02:46 -0700

    Add new yesterday image feature

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 export default function SideBar(props) {
-    const { handleToggleModal, data } = props
+    const { handleToggleModal, data, changeDateToYesterday } = props;
     return (
         <div className="sidebar">
             <div onClick={handleToggleModal} className="bgOverlay"></div>
@@ -9,6 +9,7 @@ export default function SideBar(props) {
                     <p className="descriptionTitle">{data?.date}</p>
                     <p>{data?.explanation}</p>
                 </div>
+                <button onClick={changeDateToYesterday}>Get Yesterday’s Image</button>
                 <button onClick={handleToggleModal}>
                     <i className="fa-solid fa-arrow-right"></i>
                 </button>
